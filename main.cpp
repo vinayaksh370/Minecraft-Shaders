9605d51606d13d3aa49fff801bd0c2d444f31af8
Vinayak <vinayaksh370@gmail.com>
2026-05-31 21:14:52 +0530

    7. Resolved main errors

--- main.cpp
+++ main.cpp
@@ -4,9 +4,20 @@ int add(int a, int b){
     int c = a + b;
     return c;
 }
+int sub(int a, int b){
+    int c = a - b;
+    return c;
+}
+int mul(int a, int b){
+    int c = a * b;
+    return c;
+}
 
 int main()
 {
     std::cout << "Print me daddy";
     std::cout << add(2,5);
+    std::cout << "Print me mommy"
+    std::cout << sub(2,5);
+    std::cout << mul(2,5);
 }
